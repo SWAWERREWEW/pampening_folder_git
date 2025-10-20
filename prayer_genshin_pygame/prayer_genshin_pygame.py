ff764ed2ee0faa4e59f209bd737a1f145e4614da
Korobeynikov Artem Yuryevich <113207572+SWAWERREWEW@users.noreply.github.com>
2025-10-20 19:34:16 +0500

    Add files via upload

---
+++ prayer_genshin_pygame/prayer_genshin_pygame.py
@@ -0,0 +1,30 @@
+import pygame
+import _other.cons
+import _other.objectss
+
+pygame.init()
+
+run = True
+
+f = 0
+
+clock = pygame.time.Clock()
+
+
+while run:
+
+    _other.cons.scr.fill(pygame.Color("green"))
+
+    _other.cons.scr.blit(_other.cons.my_font.render(str(_other.objectss.five()), False, "black"), (400, 400))
+
+    _other.objectss.button_prayer_10()
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            run = False
+            exit()
+
+    clock.tick(_other.cons.fps)
